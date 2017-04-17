df11498a0fccfc818f7807fbf2560a3ed5c8ebf2
JanJulius <lang1600@student.nhl.nl>
2017-04-17 21:47:56 +0200

    Moved and deleted some things  also added some new classes

---
+++ Unity Project/Unity Project/Assets/Scripts/Utilities/Game/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    /// <summary>
+    /// Represents the name of the game.
+    /// </summary>
+    public static string NAME = "Game_Name";
+
+    /// <summary>
+    /// Represents wether the game is in debug mode or not
+    /// </summary>
+    public static bool DEBUG_MODE = false;
+}
